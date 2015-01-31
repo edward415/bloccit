6cf9a12e0e901c3d5bbc3333024d1627caa27397
edc415 <edc415@outlook.com>
2015-01-31 09:01:28 +0000

    finished user auth implementation with Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128035250) do
+ActiveRecord::Schema.define(version: 20150130033602) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -42,9 +42,10 @@ ActiveRecord::Schema.define(version: 20150128035250) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
