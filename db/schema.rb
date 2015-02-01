3306f1ba345bc4ad57f8ef3e2fb5e13bbc9f08bc
edc415 <edc415@outlook.com>
2015-02-01 17:24:33 +0000

    added name column to user model in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130033602) do
+ActiveRecord::Schema.define(version: 20150201171820) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150130033602) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "name"
   end
 
 end
