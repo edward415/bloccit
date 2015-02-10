030d3623a2bdbfdca27b2c1c0a2dd0686e1bdb44
edc415 <edc415@outlook.com>
2015-02-10 17:53:21 +0000

    added delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,19 @@ class PostsController < ApplicationController
     end
    end
    
+   def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.destroy
+        flash[:notice] = "Post was successfully deleted."
+        redirect_to @topic
+    else
+        flash[:error] = "Error deleting post, please try again."
+        render :show
+    end
+end
+   
    private
    
    def post_params
