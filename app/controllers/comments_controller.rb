779700dedd7d4ead899dc42a37a8bdacbe6b05bc
edc415 <edc415@outlook.com>
2015-02-10 18:19:29 +0000

    shallow nesting for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
     
     @comment = current_user.comments.build(params.require(:comment).permit(:body))
@@ -9,7 +8,7 @@ class CommentsController < ApplicationController
     
     if @comment.save
       flash[:notice] = "Comment was created."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Error saving comment, please try again."
       render 'posts/show'
@@ -17,17 +16,16 @@ class CommentsController < ApplicationController
   end
   
   def destroy
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
+     @post = Post.find(params[:post_id])
      @comment = @post.comments.find(params[:id])
  
      authorize @comment
      if @comment.destroy
        flash[:notice] = "Comment was removed."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      else
        flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      end
    end
   
