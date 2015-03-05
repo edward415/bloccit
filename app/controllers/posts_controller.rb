817e8a2f508552f5011fa8a4ca88623e3b6fb317
edc415 <edc415@outlook.com>
2015-03-05 02:52:59 +0000

    mocking with r spec checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
      @post = current_user.posts.build(post_params)
      @post.topic = @topic
       if @post.save
+        @post.create_vote
         flash[:notice] = "Post was saved."
         redirect_to [@topic, @post]
       else
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,26 @@
 class Post < ActiveRecord::Base
+    # t.string   "title"
+    # t.text     "body"
+    # t.datetime "created_at"
+    # t.datetime "updated_at"
+    # t.integer  "user_id"
+    # t.integer  "topic_id"
+    # t.string   "picture"
+    # t.float    "rank"
+
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
     belongs_to :user
     belongs_to :topic
     mount_uploader :picture, ImageUploader
+    
+    default_scope { order('rank DESC') }
+    
+    validates :title, length: {minimum: 5 }, presence: true
+    validates :body, length: {minimum: 20 }, presence: true
+    validates :topic, presence: true
+    validates :user, presence: true
+    
     
     def up_votes
         votes.where(value: 1).count
@@ -18,16 +35,6 @@ class Post < ActiveRecord::Base
     end
         
     
-    default_scope { order('rank DESC') }
-    
-    validates :title, length: {minimum: 5 }, presence: true
-    validates :body, length: {minimum: 20 }, presence: true
-    #validates :topic, presence: true
-    #validates :user, presence: true
-    
-    after_create :create_vote
-    
-    
     def update_rank
         age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
         new_rank = points + age_in_days
@@ -35,7 +42,6 @@ class Post < ActiveRecord::Base
         update_attribute(:rank, new_rank)
     end
     
-    private
     
     def create_vote
         user.votes.create(value:1, post: self)
